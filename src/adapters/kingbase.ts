5aa10d121e6e682029fbe63d70ba28f1e5a1beaa
wangyubin <joyous-coder@qq.com>
2026-07-23 21:56:42 +0800

    refactor(kingbase): extend BaseAdapter

--- src/adapters/kingbase.ts
+++ src/adapters/kingbase.ts
@@ -8,8 +8,8 @@
  */
 
 import pg from 'pg';
+import { BaseAdapter } from './base.js';
 import type {
-  DbAdapter,
   QueryResult,
   SchemaInfo,
   TableInfo,
@@ -23,7 +23,7 @@ import { withRetry } from '../utils/retry.js';
 
 const { Pool } = pg;
 
-export class KingbaseAdapter implements DbAdapter {
+export class KingbaseAdapter extends BaseAdapter {
   private pool: pg.Pool | null = null;
   private config: {
     host: string;
@@ -40,6 +40,7 @@ export class KingbaseAdapter implements DbAdapter {
     password?: string;
     database?: string;
   }) {
+    super();
     this.config = config;
   }
 
@@ -470,4 +471,8 @@ export class KingbaseAdapter implements DbAdapter {
   isWriteOperation(query: string): boolean {
     return checkWriteOperation(query);
   }
+  protected getDialect(): import('../utils/adapter-factory.js').DbType {
+    return 'kingbase';
+  }
+
 }
